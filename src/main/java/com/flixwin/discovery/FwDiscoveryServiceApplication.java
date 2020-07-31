3751a37f9a3a30748358cabfdccaa325395f3076
vikas <vikas@vikas-Vostro-3480>
2020-07-31 20:12:29 +0530

    discovery maven maven project

---
+++ src/main/java/com/flixwin/discovery/FwDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flixwin.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FwDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FwDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flixwin/discovery/FwDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flixwin.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FwDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
