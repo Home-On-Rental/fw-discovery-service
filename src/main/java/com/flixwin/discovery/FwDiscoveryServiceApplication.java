74592f3e223373fb83876c92aa14612f5825f7c4
vikas <vikas@vikas-Vostro-3480>
2020-07-31 22:01:40 +0530

    add basic setup to run service

--- src/main/java/com/flixwin/discovery/FwDiscoveryServiceApplication.java
+++ src/main/java/com/flixwin/discovery/FwDiscoveryServiceApplication.java
@@ -2,8 +2,10 @@ package com.flixwin.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class FwDiscoveryServiceApplication {
 
 	public static void main(String[] args) {
